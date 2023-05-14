c651e7d5f12c37d58793928e571381bd180761ec
Chawasit Tengtrairatana <chawasit555@gmail.com>
2023-05-14 17:00:27 +0700

    styling

--- src/components/GreetingAndConnect.vue
+++ src/components/GreetingAndConnect.vue
@@ -7,9 +7,14 @@ const meterStore = useMeterStore();
 <template>
   <div class="container">
     <h1 class="title is-2">
-      Agtron
+      <div class="icon-text">
+        <span class="icon has-text-success">
+          <i class="fa-brands fa-cloudscale"></i>
+        </span>
+        <span>Agtron</span>
+      </div>
     </h1>
-    <h2 class="subtitle is-4">
+    <h2 class="subtitle is-4 mt-1">
       Where data and consistency meet, creating the perfect roasted coffee.
     </h2>
     <br>
--- src/components/TheMeter.vue
+++ src/components/TheMeter.vue
@@ -10,14 +10,11 @@ onMounted(() => {
 })
 
 const agtronScale = computed(() => {
-  if (meterStore.agtron == 0) return "Ready!"
-
   return meterStore.agtron
 })
 
 const agtronScaleDescription = computed(() => {
-  if (meterStore.agtron == 0) return "Please load your Sample."
-
+  if (meterStore.agtron <= 20) return "Overdeveloped"
   if (meterStore.agtron <= 30) return "SCAA: Very Dark (Italian)"
   if (meterStore.agtron < 40) return "SCAA: Dark (French)"
   if (meterStore.agtron < 50) return "SCAA: Medium Dark (Full City)"
@@ -29,48 +26,83 @@ const agtronScaleDescription = computed(() => {
 
   return "Underdeveloped"
 })
+
+const agtronScaleColor = computed(() => {
+  if (meterStore.agtron <= 20) return "#0B0806"
+  if (meterStore.agtron <= 30) return "#160F0D"
+  if (meterStore.agtron < 40) return "#211713"
+  if (meterStore.agtron < 50) return "#2C1E19"
+  if (meterStore.agtron < 60) return "#362620"
+  if (meterStore.agtron < 70) return "#412E26"
+  if (meterStore.agtron < 80) return "#4C352C"
+  if (meterStore.agtron < 90) return "#573D32"
+  if (meterStore.agtron < 100) return "#624439"
+
+  return "#6D4C3F"
+})
 </script>
 
 <template>
   <div class="container is-fluid">
     <div class="columns is-vcentered">
       <div class="column is-5 ">
-        <div class="has-text-centered" v-if="meterStore.meterState == 0">
+        <div class="is-align-items-center is-flex is-flex-direction-column" v-if="meterStore.meterState == 0">
           <h1 class="title is-2">
-            Setting Up!
+            <div class="icon-text">
+              <span class="icon has-text-info">
+                <i class="fa-solid fa-gear fa-spin"></i>
+              </span>
+              <span>Setting Up</span>
+            </div>
           </h1>
           <h2 class="subtitle is-4 has-text-grey-light">
             Preparing liquid oxygen.
           </h2>
         </div>
 
-        <div class="has-text-centered" v-if="meterStore.meterState == 1">
+        <div class="is-align-items-center is-flex is-flex-direction-column" v-if="meterStore.meterState == 1">
           <h1 class="title is-2">
-            Warm Up LED
+            <div class="icon-text">
+              <span class="icon has-text-danger">
+                <i class="fa-solid fa-fire"></i>
+              </span>
+              <span>Warm Up</span>
+            </div>
           </h1>
           <h2 class="subtitle is-4 has-text-grey-light">
             Preparing for ignition.
           </h2>
         </div>
 
-        <div class="has-text-centered" v-if="meterStore.meterState == 2">
+        <div class="is-align-items-center is-flex is-flex-direction-column" v-if="meterStore.meterState == 2">
           <h1 class="title is-2">
-            Ready!
+            <div class="icon-text">
+              <span class="icon has-text-success">
+                <i class="fa-solid fa-check-double"></i>
+              </span>
+              <span>Ready!</span>
+            </div>
           </h1>
           <h2 class="subtitle is-4 has-text-grey-light">
-            Please load your sample!
+            Please load your sample.
           </h2>
         </div>
 
-        <div class="has-text-centered" v-if="meterStore.meterState == 3">
+        <div class="is-align-items-center is-flex is-flex-direction-column" v-if="meterStore.meterState == 3">
           <h1 class="title is-2">
-            {{ agtronScale }}
+            <div class="icon-text">
+              <span class="icon" :style="[{ color: agtronScaleColor }]">
+                <!-- <i class="fa-solid fa-spinner fa-spin fa-pulse"></i> -->
+                <i class="fa-solid fa-compass fa-spin"></i>
+              </span>
+              <span class="mx-2">{{ agtronScale }}</span>
+            </div>
           </h1>
           <h2 class="subtitle is-4 has-text-grey-light">
             {{ agtronScaleDescription }}
           </h2>
           <h3 class="subtitle is-4 has-text-grey-light">
-            {{ meterStore.particleSensor }}
+            Raw: {{ meterStore.particleSensor }}
           </h3>
         </div>
       </div>
